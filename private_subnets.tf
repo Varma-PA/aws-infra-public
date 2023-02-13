472bccf72b5659971250c0d03ec98626b9c91818
Achyuth Varma <penmetcha.s@northeastern.edu>
2023-02-13 00:59:19 -0500

    Added public and private subnets

---
+++ private_subnets.tf
@@ -0,0 +1,43 @@
+resource "aws_subnet" "private_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.subnet_1_private_cidr
+
+  availability_zone = var.availability_zones[0]
+
+  tags = {
+    "Name" = "Terraform Private Subnet 1"
+  }
+
+}
+
+
+resource "aws_subnet" "private_2" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.subnet_2_private_cidr
+
+  availability_zone = var.availability_zones[1]
+
+  tags = {
+    "Name" = "Terraform Private Subnet 2"
+  }
+
+}
+
+resource "aws_subnet" "private_3" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.subnet_3_private_cidr
+
+
+  availability_zone = var.availability_zones[2]
+
+  tags = {
+    "Name" = "Terraform Private Subnet 3"
+  }
+
+}
---
+++ public_subnets.tf
@@ -0,0 +1,44 @@
+resource "aws_subnet" "public_1_az_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.subnet_1_public_cidr
+
+  availability_zone = var.availability_zones[0]
+
+  tags = {
+    "Name" = "Terraform Public Subnet 1"
+  }
+
+}
+
+
+resource "aws_subnet" "public_2_az_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.subnet_2_public_cidr
+
+  availability_zone = var.availability_zones[1]
+
+  tags = {
+    "Name" = "Terraform Public Subnet 2"
+  }
+
+}
+
+resource "aws_subnet" "public_3_az_1" {
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = var.subnet_3_public_cidr
+
+
+  availability_zone = var.availability_zones[2]
+
+  tags = {
+    "Name" = "Terraform Public Subnet 3"
+  }
+
+}
+
